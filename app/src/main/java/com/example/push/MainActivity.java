ade3c6e92a23eb6bfb37a63ca51160d4521f91a1
haotran12380 <62280123+haotran12380@users.noreply.github.com>
2020-03-17 20:02:38 +0700

    commit 1

---
+++ app/src/main/java/com/example/push/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.push;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // a lon r cai e up la cai tren a
+    private void  TestUpload(){
+        int i = 1+1;
+    }
+}
